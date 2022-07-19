4e758486129ea3dcd2d5aa82326e93650e981762
Skittlq <skittlq@gmail.com>
2022-07-20 07:29:12 +1000

    working navbar, theme choice saves

--- dark-light-mode.js
+++ dark-light-mode.js
@@ -1,14 +1,16 @@
-  function toggleTheme() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementsByTagName("link")[0];
+function toggleTheme() {
+const theme = document.getElementsByTagName('link')[0];
+const lightTheme = 'light.css';
+const darkTheme = 'dark.css';
+const newTheme = theme.getAttribute('href') === lightTheme ? darkTheme : lightTheme;
+theme.setAttribute('href', newTheme);
+// set new theme as preferred
+localStorage.setItem('theme', newTheme);
+}
 
-    // Change the value of href attribute
-    // to change the css sheet.
-    if (theme.getAttribute("href") == "light.css") {
-      theme.setAttribute("href", "dark.css");
-    } else {
-      theme.setAttribute("href", "light.css");
-    }
-  }
+const theme = document.getElementsByTagName('link')[0];
+const themeValue = localStorage.getItem('theme');
+if (themeValue) {
+theme.setAttribute('href', themeValue);
+}
+
