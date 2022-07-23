a35acd23bcf9444a40ff8296a54225216c00f6af
Skittlq <skittlq@gmail.com>
2022-07-24 09:42:43 +1000

    made code easier to read, now has mobile support

--- dark-light-mode.js
+++ dark-light-mode.js
@@ -1,17 +1,17 @@
 function toggleTheme() {
-const theme = document.getElementsByTagName('link')[0];
-const lightTheme = 'light.css';
-const darkTheme = 'dark.css';
-const newTheme = theme.getAttribute('href') === lightTheme ? darkTheme : lightTheme;
-theme.setAttribute('href', newTheme);
-// set new theme as preferred
-localStorage.setItem('theme', newTheme);
+    const theme = document.getElementsByTagName('link')[0];
+    const lightTheme = 'light.css';
+    const darkTheme = 'dark.css';
+    const newTheme = theme.getAttribute('href') === lightTheme ? darkTheme : lightTheme;
+    theme.setAttribute('href', newTheme);
+    // set new theme as preferred
+    localStorage.setItem('theme', newTheme);
 }
 
 const theme = document.getElementsByTagName('link')[0];
 const themeValue = localStorage.getItem('theme');
 if (themeValue) {
-theme.setAttribute('href', themeValue);
+    theme.setAttribute('href', themeValue);
 }
 
 
