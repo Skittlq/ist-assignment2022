e4aa2fe19b903b3c0f453ee6af957e644897ae08
Skittlq <skittlq@gmail.com>
2022-07-30 17:32:57 +1000

    fixed background video, added more comments in code, text changes when clicking light/dark mode button

--- dark-light-mode.js
+++ dark-light-mode.js
@@ -2,10 +2,10 @@ function toggleTheme() {
     const theme = document.getElementsByTagName('link')[0];
     const lightTheme = "/ist-assignment2022/light.css";
     const darkTheme = "/ist-assignment2022/dark.css";
-    const newTheme = theme.getAttribute('href') === lightTheme ? darkTheme : lightTheme;
-    theme.setAttribute('href', newTheme);
-    // set new theme as preferred
-    localStorage.setItem('theme', newTheme);
+    const newTheme = theme.getAttribute('href') === lightTheme ? darkTheme : lightTheme; // If theme is light mode, then set newTheme as lightTheme, if not, then set newTheme as darkTheme.
+
+    theme.setAttribute('href', newTheme);    // Set theme as the one chosen by newTheme variable.
+    localStorage.setItem('theme', newTheme); // Save theme choice in local storage.
 }
 
 const theme = document.getElementsByTagName('link')[0];
@@ -14,4 +14,18 @@ if (themeValue) {
     theme.setAttribute('href', themeValue);
 }
 
+function toggleTextTheme() {
+    var x = document.getElementById("dark-light-button");                                   //
+    const lightTheme = "/ist-assignment2022/light.css";                                     //
+    const darkTheme = "/ist-assignment2022/dark.css";                                       // A bunch of
+    const newTheme = theme.getAttribute('href') === lightTheme ? darkTheme : lightTheme;    // VARIABLES
+    const lightmode = "Toggle Light Mode";                                                  //
+    const darkmode = "Toggle Dark Mode";                                                    //
 
+    if (newTheme === lightTheme) {  // Checks if the theme is light mode.
+        x.innerHTML = lightmode;    // Displays Toggle Dark Mode text.
+    }
+    if (newTheme === darkTheme) {   // Checks if the theme is dark mode.
+        x.innerHTML = darkmode;     // Displays Toggle Light Mode text.
+    }
+}
